521d1e34a32cf65c99462ee68c839389042e1768
hikaru-y <hkrysg@gmail.com>
2023-07-11 05:58:45 +0900

    test

--- tools/run-qt5.15.bat
+++ tools/run-qt5.15.bat
@@ -3,7 +3,7 @@ pushd "%~dp0\.."
 
 set PYTHONWARNINGS=default
 set PYTHONPYCACHEPREFIX=out\pycache
-set ANKIDEV=1
+set ANKIDEV=2
 
 call tools\ninja pylib qt pyenv-qt5.15 || exit /b 1
 .\out\pyenv-qt5.15\scripts\python tools\run.py %* || exit /b 1
